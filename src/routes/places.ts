c4038e63aa265b60380b224e8acfa961122d6d42
Conor Kingston <cmkingston21@me.com>
2020-11-23 15:59:12 -0700

    add Image dependency

--- src/routes/places.ts
+++ src/routes/places.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import {getRepository, FindManyOptions} from "typeorm";
 import {Place} from "../entity/Place";
+import {Image} from '../entity/Image';
 
 const router = express.Router()
 const placesRepository = () => getRepository(Image)
