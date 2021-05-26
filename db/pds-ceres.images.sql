4169962a4ab93fb36183400b984b24639b5f4e4b
Conor Kingston <cmkingston21@me.com>
2021-05-26 14:52:44 -0700

    updated ceres sql import

--- db/pds-ceres.images.sql
+++ db/pds-ceres.images.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed1ff6a5447967dffabff6aa28c699a0c486b3e0e4a4202f1f9a8c756751bf95
-size 49168435
+oid sha256:096e2ab3542dd226eff145ec895977b9d5a27728a962fa1ee4584143055868a4
+size 48661230
