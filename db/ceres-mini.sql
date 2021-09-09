97e64a645f9f920c883d5612d66bfc80cb459a86
Conor Kingston <cmkingston21@me.com>
2021-09-09 12:19:13 -0700

    testing sql files

---
+++ db/ceres-mini.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6627db465b22ca681424e0a58a49fa6cd76b9135ca1939c8a43c75e42828e99e
+size 3705
---
+++ db/foot.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb49aff07f41543b1b0f2d3f89348b34e5c64ae1488a8357cb2109d6dc4cd742
+size 2783
